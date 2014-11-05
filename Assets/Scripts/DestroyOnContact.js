c17dc61d51433a57d07f8542dfab68d245eeb40d
Subhash A <s.a@sap.com>
2014-11-05 12:07:24 +0530

    added the scoring text and logic.

--- Assets/Scripts/DestroyOnContact.js
+++ Assets/Scripts/DestroyOnContact.js
@@ -1,10 +1,24 @@
 ﻿#pragma strict
 
+var scoreValue: int;
+private var gameController: GameController;
+
+function Start() {
+	var gameControllerObject : GameObject = GameObject.FindGameObjectWithTag("GameController");
+	if(gameControllerObject !== null) {
+		gameController = gameControllerObject.GetComponent(GameController);
+	}
+	if(gameControllerObject === null) {
+		Debug.Log("Game Controller Object not found");
+	}
+}
 function OnTriggerEnter(other: Collider) {
     if(other.tag == "Boundary") {
     	return;
     }
+    gameController.addScore(scoreValue);
     Destroy(other.gameObject);
+	
 	Destroy(gameObject);
 	
 }
--- Assets/Scripts/GameController.js
+++ Assets/Scripts/GameController.js
@@ -2,13 +2,38 @@
 
 var spawnValues: Vector3;
 var hazard: GameObject;
+var hazardCount: int;//Number of Hazards to spawn
+var spawnWait: float;//Wait for these many seconds before spawing next asteriod
+var waveWait: float;//Wait for these many seconds before spawning next wave
+var startWait: float;//Wait for these many seconds before starting the asteriod wave
+var loopCount: int;
+var scoreText: GUIText;
+var score: int;
 
 function SpawnWaves() {
-	var spawnPosition = new Vector3(spawnValues.x, spawnValues.y, spawnValues.z);
-	var spawnRotation = new Quaternion();
-	Instantiate(hazard, spawnPosition, spawnRotation);
+	yield WaitForSeconds(startWait);
+	while(true) {
+		for(loopCount = 0; loopCount < hazardCount; loopCount++) {
+			var spawnPosition = new Vector3(Random.Range(-spawnValues.x,spawnValues.x), spawnValues.y, spawnValues.z);
+			var spawnRotation = Quaternion.identity;
+			Instantiate(hazard, spawnPosition, spawnRotation);
+			yield WaitForSeconds(spawnWait);
+		}
+		yield WaitForSeconds(waveWait);
+	};
+}
+
+function addScore(newScore: int) {
+	score = score + newScore;
+	updateScore();
+}
+
+function updateScore() {
+	scoreText.text = "Score: " + score;
 }
 
 function Start() {
+	score = 0;
+	updateScore();
 	SpawnWaves();
 }
